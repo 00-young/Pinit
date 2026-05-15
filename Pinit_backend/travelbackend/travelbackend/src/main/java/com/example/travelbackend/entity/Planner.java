4a61b3e187d8c45edb487de663f1ca70cde6d869
JihoonPark <qwertop860@gmail.com>
2026-05-15 21:41:46 +0900

    백엔드 SpringBoot + MySQL 폴더 추가 및 깃 파일 올리기 연습. 사용할 때 application.properties에서 password 자신의 것으로 변경해야 할 수도 있습니다.

---
+++ Pinit_backend/travelbackend/travelbackend/src/main/java/com/example/travelbackend/entity/Planner.java
@@ -0,0 +1,32 @@
+package com.example.travelbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Planner")
+@Getter
+@Setter
+public class Planner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "plannerNumber")
+    private Integer plannerNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "userNumber")
+    private User user;
+
+    @Column(name = "firstDate")
+    private java.time.LocalDate firstDate;
+
+    @Column(name = "lastDate")
+    private java.time.LocalDate lastDate;
+
+    @Column(name = "plannerTitle")
+    private String plannerTitle;
+
+    @Column(name = "plannerHit")
+    private Integer plannerHit;
+}
