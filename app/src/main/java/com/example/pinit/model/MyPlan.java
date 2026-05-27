1a1c3c5730c2f4fa8ff8debb170cc64d11226128
minseo993 <jomins04@naver.com>
2026-05-28 01:06:31 +0900

    검색결과 정렬 기능+post detail 추가

--- app/src/main/java/com/example/pinit/model/MyPlan.java
+++ app/src/main/java/com/example/pinit/model/MyPlan.java
@@ -9,6 +9,9 @@ public class MyPlan implements Serializable {
     private String country;
     private List<DailySchedule> schedules;
 
+    public MyPlan() {
+    }
+
     public MyPlan(String title, String date, String country, List<DailySchedule> schedules) {
         this.title = title;
         this.date = date;
@@ -20,4 +23,9 @@ public class MyPlan implements Serializable {
     public String getDate() { return date; }
     public String getCountry() { return country; }
     public List<DailySchedule> getSchedules() { return schedules; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setDate(String date) { this.date = date; }
+    public void setCountry(String country) { this.country = country; }
+    public void setSchedules(List<DailySchedule> schedules) { this.schedules = schedules; }
 }
